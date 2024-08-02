bc1acdb657243e3c086fbe78fbde626d2ef6ad35
Marcus Benoit <benoit.marcus@gmail.com>
2024-08-02 21:30:05 +0200

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Marcus Benoit on 02.08.24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
