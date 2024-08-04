5d1597c1d7968211a462d32646d62df62b5188f4
Marcus Benoit <benoit.marcus@gmail.com>
2024-08-04 14:53:52 +0200

    splitting examples into seperate views

---
+++ HotProspects/Notifications.swift
@@ -0,0 +1,18 @@
+//
+//  Notifications.swift
+//  HotProspects
+//
+//  Created by Marcus Benoit on 04.08.24.
+//
+
+import SwiftUI
+
+struct Notifications: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Notifications()
+}
---
+++ HotProspects/TabViewExample.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewExample.swift
+//  HotProspects
+//
+//  Created by Marcus Benoit on 04.08.24.
+//
+
+import SwiftUI
+
+struct TabViewExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewExample()
+}
